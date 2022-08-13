bd8079fb744e79fa40773c9d6aaa50e0d3f160b9
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-12 21:45:33 -0300

    Create Hello World.java

---
+++ Aula 1/Hello World.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Richard - Primeiro Programa Java
+12/08/2022
+
+temos aqui:
+1 método chamado main (métodos com letra minúscula)
+Nenhum atributo
+1 classe chamda Main (classes com letra maiúscula)
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
