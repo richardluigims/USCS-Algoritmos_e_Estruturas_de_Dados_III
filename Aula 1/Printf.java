cb592d10ba5fc14ebee5040474d3c9808a180c29
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-12 21:47:52 -0300

    Create Printf.java

---
+++ Aula 1/Printf.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Richard - Primeiro Programa Java
+12/08/2022
+
+temos aqui:
+1 método chamado main (métodos com letra minúscula)
+Nenhum atributo
+1 classe chamda Main (classes com letra maiúscula)
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int a = 4, b = 2;
+	    
+		System.out.println("A = " + a + " B = " + b); // printf
+	}
+}
