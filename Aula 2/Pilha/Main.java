e105db92e3ca335d1409d97d4a493cf66a9a429e
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-19 21:40:32 -0300

    Create Main.java

---
+++ Aula 2/Pilha/Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Nome: Richard
+Data: 19/08/2022
+
+Exemplo de pilha
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args)
+	{
+		TadPilha p = new TadPilha (5);
+		
+		p.empilha(1);
+		p.empilha(2);
+		p.imprime();
+	}
+}
