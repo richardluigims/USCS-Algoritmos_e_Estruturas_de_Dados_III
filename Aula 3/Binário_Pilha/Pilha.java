4dec56516d3353cd09a387d3f215cd056ca0e381
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-08-26 22:12:38 -0300

    Create Pilha.java

---
+++ Aula 3/Binário_Pilha/Pilha.java
@@ -0,0 +1,50 @@
+public class Pilha
+{
+    int capacidade;
+    int topo;
+    int dados[];
+    
+    void empilha (int d)
+    {
+        if (!cheia())
+        {
+            topo++;
+            dados[topo] = d;
+        }
+    }
+    
+    boolean cheia()
+    {
+        return (capacidade == topo + 1);
+    }
+    
+    void imprime ()
+    {
+        for (int i = 0; i <= topo; i++)
+        {
+            System.out.println(dados[i]);
+        }
+    }
+    
+    void imprimeAoContrario ()
+    {
+        for (int i = topo; i >= 0; i--)
+        {
+            System.out.print(dados[i] + " ");
+        }
+    }
+    
+    public Pilha (int c)
+    {
+        capacidade = c;
+        topo = -1;
+        dados = new int [c];
+    }
+    
+    public Pilha ()
+    {
+        capacidade = 100;
+        topo = -1;
+        dados = new int [100];
+    }
+}
