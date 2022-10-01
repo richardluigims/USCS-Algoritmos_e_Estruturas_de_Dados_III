9ba8ac27ce5f37c5863f082be6918f6181ef6cba
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-09-30 21:27:30 -0300

    Create Pessoa.java

---
+++ Aula 7/Fila Circular/Pessoa.java
@@ -0,0 +1,18 @@
+class Pessoa
+{
+    int idade;
+    char sexo;
+    String nome;
+    
+    public Pessoa (int i, char s, String n)
+    {
+        idade = i;
+        sexo = s;
+        nome = n;
+    }
+    
+    void imprime ()
+    {
+        System.out.println("Nome: " + nome + ". Idade: " + idade + ". Sexo: " + sexo);
+    }
+}
